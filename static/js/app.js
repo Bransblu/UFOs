7920daaa2bba6c4c94a3f7b3d564f91f8df573dc
Bransblu <brandon.p.driver@gmail.com>
2022-06-02 10:09:14 -0700

    Update app.js to include table.

--- static/js/app.js
+++ static/js/app.js
@@ -10,11 +10,14 @@ function buildTable(data) {
 
 // Chained for loop with argument "dataRow" that will iterate through array.
 data.forEach((dataRow) => {
+
     // Finding <tbody> tag and adding table row tr.
     let row = tbody.append("tr");
+
     // Reference one object from UFA sighting. Add value to dataRow
     // forEach(val) specifies one object per row.
     Object.values(dataRow).forEach((val) => {
+
         // Appending data into table data tag.
         let cell = row.append("td");
         // Add values from object.
@@ -22,3 +25,22 @@ data.forEach((dataRow) => {
         }
     );
 });
+
+function handleClick() { 
+    let date = d3.select("#datetime").property("value");
+
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+       // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+buildTable(tableData);
